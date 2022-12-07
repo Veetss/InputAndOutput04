543f52ccd507a4e4053a2fed8bc3c9fde40affa5
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:10:08 +0100

    Update PrintYourNameAndPi.java

--- src/PrintYourNameAndPi.java
+++ src/PrintYourNameAndPi.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class PrintYourNameAndPi {
+public class PrintYourNameAndPi {
+     static String a = "VITTORIA";
+     static double b = 3.141590;
+
+    public static void main(String[] args) {
+        System.out.printf("%s\nThe number of Pi is: %f", a, b);
+    }
 }
